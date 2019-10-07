404853ec913fd2d22387c25a490273f04da58bcd
AJ Harvey <andrew@cpmed.io>
2019-10-07 15:43:07 -0400

    Add Jenny's code

--- client/src/components/Frame.tsx
+++ client/src/components/Frame.tsx
@@ -1,58 +1,150 @@
-import React, {CSSProperties} from 'react';
+import React, {CSSProperties, useState, useRef, useContext, useEffect} from 'react';
 
 type Props = {
     header: string,
     fontSize?: number,
-    headerLeft?: React.ReactNode,
-    headerRight?: React.ReactNode,
+    SideMenu?: React.ReactNode,
+    ShowSideMenu?: boolean,
 };
 
-const Frame: React.FC<Props> = ({header,fontSize,headerLeft,headerRight,children}) => {
+const Frame: React.FC<Props> = ({header,fontSize,SideMenu,ShowSideMenu,children}) => {
     
+
     return (
         <div style={styles.appContainer}>
             <div style={{...styles.header, fontSize: fontSize || 45}}>
-                <div style={styles.headerLeft}>
+                {header}
+                {/* <div style={styles.headerLeft}>
                     {headerLeft}
                 </div>
-                {header}
                 <div style={styles.headerRight}>
                     {headerRight}
-                </div>
+                </div> */}
             </div>
+
+            
+            <div style={styles.sideBar}>
+                    <div style={styles.sideBarTop}>
+                        Export 
+                        Import
+                        Close X
+                    </div>
+                    <div style={styles.sideBarContent}>
+                        <div style={styles.line}>
+                            <div style={styles.name}>Staff Scale</div>
+                            <div style={styles.option}>Medium </div>
+                        </div>
+
+                        <div style={styles.line}>
+                            <div style={styles.name}>Horizontal Spacing</div>
+                            <div style={styles.option}>10 px </div>
+                        </div>
+
+                        <div style={styles.line}>
+                            <div style={styles.name}>Vertical Spacing</div>
+                            <div style={styles.option}> 20 px </div>
+                        </div>
+
+
+                    </div>
+            </div>
+
+
             <div style={styles.page}>
                 {children}
+
+                
             </div>
         </div>
     );
 };
 
 const styleMap = {
+    name:{
+        position:'relative',
+        width: '50%',
+        paddingTop: '6%',
+    },
+    option:{
+        paddingTop: '5%',
+        background: '#F5F5F5',
+        border: '1px solid #6F6F6F',
+        boxSizing: 'border-box',
+        borderRadius: '10px',
+        position:'relative',
+        width: '50%',
+        textAlign:'center',
+
+    },
+    line:{
+
+        margin: '30px 0px',
+        justifyContent: 'center',
+        alignItems: 'center',
+        display: 'flex',
+        position:'relative',
+        height:'50px',
+
+
+    },
+    sideBarContent:{
+        padding: '0 20px',
+        position:'relative',
+        marginTop: '40px',
+    },
     appContainer: {
         overflow: 'hidden',
     },
     header: {
+        // fontFamily: 'open sans',
+        fontStyle: 'normal',
+        fontWeight: 'normal',
+        fontSize: '42px',
+        alignItems: 'center',
+        textAlign: 'center',
+        letterSpacing: '0.15em',
         left: '50%',
-        height: '100px',
+        top:'0px',
+        height: '80px',
         transform: 'translate(-50%,0px)',
         backgroundColor: '#31B7D6',
-        lineHeight: '100px',
         minWidth: '600px',
-        textAlign: 'center',
+        position: 'absolute',
     },
-    headerLeft: {
-        left: '20px',
-        width: 'auto',
-    },
-    headerRight: {
-        left: 'auto',
-        right: '20px',
-        width: 'auto',
-    },
+   
     page: {
         top: '100px',
         height: 'calc(100% - 100px)',
     },
+    sideBar:{
+        top: '80px',
+        height: 'calc(100% - 100px)',
+
+        borderLeft: '1px solid #6F6F6F',
+        boxSizing: 'border-box',
+        position:'relative',
+
+        width:'20%',
+        float:'right',
+
+    },
+
+    sideBarTop:{
+        justifyContent: 'center',
+        alignItems: 'center',
+        borderBottom: '1px solid #6F6F6F',
+        boxSizing: 'border-box',
+        height: '80px',
+        display:'flex',
+        color:'#31B7D6',
+        fontSize: '25px',
+        fontWeight:'bold',
+        cursor: 'pointer',
+        position: 'relative',
+        width: 'auto',
+    }
+
+
 } as const;
 const styles: Record<keyof typeof styleMap, CSSProperties> = styleMap;
 
--- client/src/pages/Convert.tsx
+++ client/src/pages/Convert.tsx
@@ -1,12 +1,15 @@
-import React, {useEffect, useState} from 'react';
-import {RouteComponentProps} from "@reach/router";
+import React, {useEffect, useState, CSSProperties} from 'react';
+import {RouteComponentProps, navigate} from "@reach/router";
 import SNView from '../components/SNView';
+import Frame from '../components/Frame';
 import Connection from '../util/Connection';
 import MusicXML from 'musicxml-interfaces';
 
+
+
 type Props = {} & RouteComponentProps;
 
-const Menu: React.FC<Props> = () => {
+const Convert: React.FC<Props> = () => {
     let [xml,setXML] = useState<undefined | MusicXML.ScoreTimewise>(undefined);
     useEffect(()=>{
         let canceled = false;
@@ -22,20 +25,88 @@ const Menu: React.FC<Props> = () => {
     },[]);
     return (
         <>
-            {xml===undefined?null:<SNView xml={xml} />}
+
+         <Frame header="SNapp" fontSize={55}></Frame>
+            <div style={styles.leftSide} >
+                <div style={styles.subHeader}>
+
+                    <div style={styles.left} onClick={()=>{navigate('/');}}>
+                        <svg style={styles.svg} xmlns="http://www.w3.org/2000/svg" width="24" height="24" viewBox="0 0 24 24" fill="none" stroke="currentColor" stroke-width="2.5" stroke-linecap="round" stroke-linejoin="round"><path d="M3 9l9-7 9 7v11a2 2 0 0 1-2 2H5a2 2 0 0 1-2-2z"></path><polyline points="9 22 9 12 15 12 15 22"></polyline></svg>
+                        Home
+                    </div>
+
+                    <div style={styles.right}>
+                        <svg style={styles.svg} xmlns="http://www.w3.org/2000/svg" width="24" height="24" viewBox="0 0 24 24" fill="none" stroke="currentColor" stroke-width="2.5" stroke-linecap="round" stroke-linejoin="round"><circle cx="12" cy="12" r="3"></circle><path d="M19.4 15a1.65 1.65 0 0 0 .33 1.82l.06.06a2 2 0 0 1 0 2.83 2 2 0 0 1-2.83 0l-.06-.06a1.65 1.65 0 0 0-1.82-.33 1.65 1.65 0 0 0-1 1.51V21a2 2 0 0 1-2 2 2 2 0 0 1-2-2v-.09A1.65 1.65 0 0 0 9 19.4a1.65 1.65 0 0 0-1.82.33l-.06.06a2 2 0 0 1-2.83 0 2 2 0 0 1 0-2.83l.06-.06a1.65 1.65 0 0 0 .33-1.82 1.65 1.65 0 0 0-1.51-1H3a2 2 0 0 1-2-2 2 2 0 0 1 2-2h.09A1.65 1.65 0 0 0 4.6 9a1.65 1.65 0 0 0-.33-1.82l-.06-.06a2 2 0 0 1 0-2.83 2 2 0 0 1 2.83 0l.06.06a1.65 1.65 0 0 0 1.82.33H9a1.65 1.65 0 0 0 1-1.51V3a2 2 0 0 1 2-2 2 2 0 0 1 2 2v.09a1.65 1.65 0 0 0 1 1.51 1.65 1.65 0 0 0 1.82-.33l.06-.06a2 2 0 0 1 2.83 0 2 2 0 0 1 0 2.83l-.06.06a1.65 1.65 0 0 0-.33 1.82V9a1.65 1.65 0 0 0 1.51 1H21a2 2 0 0 1 2 2 2 2 0 0 1-2 2h-.09a1.65 1.65 0 0 0-1.51 1z"></path></svg>
+                        Preferences
+                    </div>
+
+                    <div style={styles.right}>
+                        <svg style={styles.svg} xmlns="http://www.w3.org/2000/svg" padding-right="5px" width="24" height="24" viewBox="0 0 24 24" fill="none" stroke="currentColor" stroke-width="2.5" stroke-linecap="round" stroke-linejoin="round"><path d="M13 2H6a2 2 0 0 0-2 2v16a2 2 0 0 0 2 2h12a2 2 0 0 0 2-2V9z"></path><polyline points="13 2 13 9 20 9"></polyline></svg>
+                        Save as PDF
+                    </div>
+
+                </div>
+                <div style={styles.snv}>
+                {xml===undefined?null:<SNView xml={xml} />}
+                </div>
+            </div>
+            
+         {/* <Btn text="TESTBnjknUTTON" action={()=>alert('f')}></Btn> */}
+
         </>
+
     );
 };
 
-// const styleMap = {
-//     boxes: {
-//         top: '200px',
-//         height: '200px',
-//         display: 'flex',
-//         flexWrap: 'nowrap',
-//         justifyContent: 'space-evenly',
-//     },
-// } as const;
-// const styles: Record<keyof typeof styleMap,CSSProperties> = styleMap;
+const styleMap = {
+    leftSide:{
+        float:'left',
+        width:'80%',
+    },
 
-export default Menu;
+    svg:{
+        marginRight:'7px',
+    },
+    snv:{
+        top: '200px',
+    },
+
+    left:{
+        display:'flex',
+        height:'auto',
+        color:'#31B7D6',
+        marginTop: '27px',
+        marginLeft: '25px',
+        fontSize: '25px',
+        fontWeight:'bold',
+        cursor: 'pointer',
+        position: 'relative',
+        float: 'left',
+        width: 'auto',
+    },
+    right:{
+        height:'auto',
+        display:'flex',
+        marginTop: '27px',
+        fontWeight:'bold',
+        color:'#31B7D6',
+        marginRight: '25px',
+        fontSize: '25px',
+        position: 'relative',
+        float: 'right',
+        width: 'auto',
+        cursor: 'pointer',
+    },
+
+    subHeader:{
+        // background:'blue',
+        height: '80px',
+        top:'80px',
+    },
+
+    
+} as const;
+const styles: Record<keyof typeof styleMap,CSSProperties> = styleMap;
+
+
+export default Convert;
